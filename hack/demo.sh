ea6fa5c9544458a0eb2090c876be8019e8006db1
Francesco Ilario <filario@redhat.com>
2024-02-19 18:09:40 +0100

    Update hack/demo.sh
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/demo.sh
+++ hack/demo.sh
@@ -16,7 +16,7 @@ cd "$f" || exit
       git clone --depth 2 git@github.com:codeready-toolchain/toolchain-e2e.git && \
       git clone --depth 2 --branch f45-demo-workspaces git@github.com:filariow/host-operator.git && \
       git clone --depth 2 --branch f45-demo-workspaces git@github.com:filariow/toolchain-common.git && \
-      git clone --depth 2 --branch f45-demo-workspaces git@github.com:filariow/toolchain-api.git api &&
+      git clone --depth 2 --branch f45-demo-workspaces git@github.com:filariow/toolchain-api.git api && \
       git clone --depth 2 --branch f45-demo-workspaces git@github.com:filariow/registration-service ) || exit 1
 
   make -C toolchain-e2e dev-deploy-e2e-local
