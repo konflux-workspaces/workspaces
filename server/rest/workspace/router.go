ae15ebb1ccf471fb474e4a6da904dd3d6665799f
Francesco Ilario <filario@redhat.com>
2024-02-22 10:41:44 +0100

    Using 1.22 mux rules
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- server/rest/workspace/router.go
+++ server/rest/workspace/router.go
@@ -6,38 +6,15 @@ import (
 )
 
 func AddWorkspaces(
-  mux *http.ServeMux,
-  prefix string,
-  c http.Handler,
-  r *ReadWorkspaceHandler,
-  l *ListWorkspaceHandler, 
-  u, d http.Handler,
+	mux *http.ServeMux,
+	prefix string,
+	c http.Handler,
+	r *ReadWorkspaceHandler,
+	l *ListWorkspaceHandler,
+	u, d http.Handler,
 ) {
-  mux.HandleFunc(prefix, func(w http.ResponseWriter, rq *http.Request) {
-    switch rq.Method {
-    case http.MethodGet:
-      l.ServeHTTP(w, rq)
 
-    default:
-      w.WriteHeader(http.StatusBadRequest)
-      w.Write([]byte(fmt.Sprintf("Method %s not supported", rq.Method)))
-    }
-  })
+	mux.Handle(fmt.Sprintf("GET %s", prefix), l)
 
-  mux.HandleFunc(fmt.Sprintf("%s/", prefix), func(w http.ResponseWriter, rq *http.Request) {
-    switch rq.Method {
-    case http.MethodGet:
-      r.ServeHTTP(w, rq)
-    case http.MethodPost:
-      c.ServeHTTP(w, rq)
-    case http.MethodPut:
-      u.ServeHTTP(w, rq)
-    case http.MethodDelete:
-      d.ServeHTTP(w, rq)
-
-    default:
-      w.WriteHeader(http.StatusBadRequest)
-      w.Write([]byte(fmt.Sprintf("Method %s not supported", rq.Method)))
-    }
-  })
+	mux.Handle(fmt.Sprintf("GET %s/{name}", prefix), r)
 }
