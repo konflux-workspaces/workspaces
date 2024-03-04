d4e0695b9225b4279ca84e4bdd0b55ebc3a31867
Francesco Ilario <filario@redhat.com>
2024-03-04 18:13:49 +0100

    Fix demo script
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/demo.sh
+++ hack/demo.sh
@@ -4,5 +4,7 @@ set -e
 
 export QUAY_NAMESPACE=${QUAY_NAMESPACE:-workspaces}
 
-( ./install_toolchain.sh )
-( ./install_workspaces.sh && make -C e2e test )
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+( "$SCRIPT_DIR/install_toolchain.sh" )
+( "$SCRIPT_DIR/install_workspaces.sh" && make -C e2e test )
