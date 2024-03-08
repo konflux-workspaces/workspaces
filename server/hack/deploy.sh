7acff12b8ad1a160abf1efa33d84b023c15c7cbf
Francesco Ilario <filario@redhat.com>
2024-03-08 22:05:05 +0100

    fix shellcheck complaints
    courtesy of @sadlerap
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- server/hack/deploy.sh
+++ server/hack/deploy.sh
@@ -2,26 +2,32 @@
 
 set -e -o pipefail
 
-ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")/../..")"
+LOCATION=$(readlink -f "$0")
+DIR=$(dirname "${LOCATION}")
+ROOT_DIR="$(realpath "${DIR}"/../..)"
 KUBECLI=${KUBECLI:-kubectl}
+KUSTOMIZE=${KUSTOMIZE:-kustomize}
 
 # retrieving toolchain-host namespace
-toolchain_host=$($KUBECLI get namespaces -o name | grep toolchain-host | cut -d'/' -f2 | head -n 1)
+toolchain_host=$(${KUBECLI} get namespaces -o name | grep toolchain-host | cut -d'/' -f2 | head -n 1)
+if [[ -z "${toolchain_host}" ]]; then
+    toolchain_host="toolchain-host_operator"
+fi
 
 # prepare temporary folder
 f=$(mktemp --directory /tmp/workspaces-rest.XXXXX)
-cp -r "$ROOT_DIR/server/manifests" "$f/manifests"
+cp -r "${ROOT_DIR}/server/manifests" "${f}/manifests"
 
 # updating manifests locally
-cd "$f/manifests/default"
-$KUSTOMIZE edit set namespace "$1"
-$KUSTOMIZE edit set image workspaces/rest-api="$2"
-$KUSTOMIZE edit add configmap rest-api-server-config \
+cd "${f}/manifests/default"
+${KUSTOMIZE} edit set namespace "$1"
+${KUSTOMIZE} edit set image workspaces/rest-api="$2"
+${KUSTOMIZE} edit add configmap rest-api-server-config \
         --behavior=replace \
-        --from-literal=kubesaw.namespace="$( [[ -n "$toolchain_host" ]] && echo "$toolchain_host" || echo "toolchain-host-operator" )"
+        --from-literal=kubesaw.namespace="${toolchain_host}"
 
 # apply manifests
-$KUSTOMIZE build . | $KUBECLI apply -f -
+${KUSTOMIZE} build . | ${KUBECLI} apply -f -
 
 # cleanup
-rm -r "$f"
+rm -r "${f}"
