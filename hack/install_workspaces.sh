566a7862786f814992ac475c61a776b5510c1dd6
Francesco Ilario <filario@redhat.com>
2024-03-09 01:13:44 +0100

    fix server deploy (#24)
    * fix server deploy
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * Move mockgen under hack
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * update clean/build targets
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * fix shellcheck complaints
    
    courtesy of @sadlerap
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * fix deployment
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * rollback mockgen go.mod and go.sum
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/install_workspaces.sh
+++ hack/install_workspaces.sh
@@ -6,7 +6,7 @@ export QUAY_NAMESPACE=${QUAY_NAMESPACE:-workspaces}
 
 f=$(mktemp --directory /tmp/workspaces-demo.XXXX)
 
-cp -r operator/ e2e/ server/ "$f"
+cp -r hack/ operator/ e2e/ server/ "$f"
 cd "$f" 
 
 make -C e2e prepare
