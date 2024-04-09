0b2ce4ff0478aa5cd33f9543c701df8b125ae292
Francesco Ilario <filario@redhat.com>
2024-04-09 16:54:35 +0200

    add toolchain build/push and install scripts (#63)
    * add toolchain build/push and install scripts
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * fix shellcheck optional checks
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * replace kubectl/oc with variable
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * fix sc2312
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ hack/toolchain_install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+# parse input
+BRANCH=${BRANCH:-pubviewer-mvp}
+export QUAY_NAMESPACE=${QUAY_NAMESPACE:-workspaces}
+TAG=${1:-e2etest}
+KUBECLI=${KUBECLI:-kubectl}
+
+# create a temporary direction
+f=$(mktemp --directory /tmp/toolchain.XXXX)
+cd "${f}"
+
+# checkout
+git clone --depth 2 https://github.com/codeready-toolchain/member-operator.git
+git clone --depth 2 --branch "${BRANCH}" https://github.com/filariow/toolchain-e2e.git
+git clone --depth 2 --branch "${BRANCH}" https://github.com/filariow/host-operator.git
+git clone --depth 2 --branch "${BRANCH}" https://github.com/filariow/toolchain-common.git
+git clone --depth 2 --branch "${BRANCH}" https://github.com/filariow/toolchain-api.git api
+git clone --depth 2 --branch "${BRANCH}" https://github.com/filariow/registration-service
+
+# deploy
+(
+  set -e -o pipefail
+
+  ${KUBECLI} create namespace toolchain-host-operator --dry-run=client --output=yaml | \
+    ${KUBECLI} apply -f -
+  ${KUBECLI} create namespace toolchain-member-operator --dry-run=client --output=yaml | \
+    ${KUBECLI} apply -f -
+
+  cd "${f}/toolchain-e2e"
+
+  make dev-deploy-e2e-local PUBLISH_OPERATOR=false DATE_SUFFIX="${TAG}" DEPLOY_LATEST=false
+)
+
+# patch configuration
+${KUBECLI} patch \
+  toolchainconfigs.toolchain.dev.openshift.com config \
+  -n toolchain-host-operator \
+  --patch='{"spec":{"global":{"publicViewer":{"enabled":true,"username":"public-viewer"}}}}' \
+  --type=merge
+
+# restart operator
+${KUBECLI} delete pods --all -n toolchain-host-operator
