90b272cd4be2e6fd03897bedae23dd7ca6735bc6
Francesco Ilario <filario@redhat.com>
2024-05-14 16:04:57 +0200

    reducing internal/cache complexity
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- server/persistence/internal/cache/cache.go
+++ server/persistence/internal/cache/cache.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
@@ -23,33 +24,35 @@ const (
 // spacebindings.toolchain.dev.openshift.com and workspaces.workspaces.io.
 // IMPORTANT: returned cache needs to be started and initialized.
 func NewCache(ctx context.Context, cfg *rest.Config, workspacesNamespace, kubesawNamespace string) (cache.Cache, error) {
-	s := runtime.NewScheme()
-	if err := corev1.AddToScheme(s); err != nil {
-		return nil, err
-	}
-	if err := metav1.AddMetaToScheme(s); err != nil {
-		return nil, err
-	}
-	if err := workspacesv1alpha1.AddToScheme(s); err != nil {
-		return nil, err
-	}
-	if err := toolchainv1alpha1.AddToScheme(s); err != nil {
-		return nil, err
-	}
-
-	hc, err := rest.HTTPClientFor(cfg)
+	s, err := createScheme()
 	if err != nil {
 		return nil, err
 	}
 
-	m, err := apiutil.NewDynamicRESTMapper(cfg, hc)
+	m, err := createMapper(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	c, err := cache.New(cfg, cache.Options{
-		Scheme:                      s,
-		Mapper:                      m,
+	c, err := newCache(cfg, s, m, workspacesNamespace, kubesawNamespace)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := c.GetInformer(ctx, &toolchainv1alpha1.SpaceBinding{}); err != nil {
+		return nil, err
+	}
+	if _, err := c.GetInformer(ctx, &workspacesv1alpha1.InternalWorkspace{}); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func newCache(cfg *rest.Config, scheme *runtime.Scheme, mapper meta.RESTMapper, workspacesNamespace, kubesawNamespace string) (cache.Cache, error) {
+	return cache.New(cfg, cache.Options{
+		Scheme:                      scheme,
+		Mapper:                      mapper,
 		ReaderFailOnMissingInformer: true,
 		ByObject: map[client.Object]cache.ByObject{
 			&toolchainv1alpha1.SpaceBinding{}:       {Namespaces: map[string]cache.Config{kubesawNamespace: {}}},
@@ -67,16 +70,30 @@ func NewCache(ctx context.Context, cfg *rest.Config, workspacesNamespace, kubesa
 			return obj, nil
 		},
 	})
+}
+
+func createMapper(cfg *rest.Config) (meta.RESTMapper, error) {
+	hc, err := rest.HTTPClientFor(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	if _, err := c.GetInformer(ctx, &toolchainv1alpha1.SpaceBinding{}); err != nil {
-		return nil, err
-	}
-	if _, err := c.GetInformer(ctx, &workspacesv1alpha1.InternalWorkspace{}); err != nil {
-		return nil, err
-	}
+	return apiutil.NewDynamicRESTMapper(cfg, hc)
+}
 
-	return c, nil
+func createScheme() (*runtime.Scheme, error) {
+	s := runtime.NewScheme()
+	if err := corev1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := metav1.AddMetaToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := workspacesv1alpha1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := toolchainv1alpha1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	return s, nil
 }
