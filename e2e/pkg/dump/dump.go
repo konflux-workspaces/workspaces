27ca4ddcde60f853698a8ceb0f845352e3736d51
Francesco Ilario <filario@redhat.com>
2024-05-14 11:48:30 +0200

    e2e: add dump hook
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ e2e/pkg/dump/dump.go
@@ -0,0 +1,57 @@
+package dump
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	yaml "sigs.k8s.io/yaml/goyaml.v3"
+
+	tcontext "github.com/konflux-workspaces/workspaces/e2e/pkg/context"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	workspacesv1alpha1 "github.com/konflux-workspaces/workspaces/operator/api/v1alpha1"
+)
+
+var resourcesToDump = func() []client.ObjectList {
+	return []client.ObjectList{
+		&workspacesv1alpha1.InternalWorkspaceList{},
+		&toolchainv1alpha1.UserSignupList{},
+		&toolchainv1alpha1.MasterUserRecordList{},
+		&toolchainv1alpha1.SpaceList{},
+		&toolchainv1alpha1.SpaceBindingList{},
+	}
+}
+
+func DumpAll(ctx context.Context) error {
+	rr := resourcesToDump()
+	for _, r := range rr {
+		dumpResourceInAllNamespaces(ctx, r)
+	}
+	return nil
+}
+
+func dumpResourceInAllNamespaces(ctx context.Context, resource client.ObjectList) error {
+	// retrieve host client
+	cli := tcontext.RetrieveHostClient(ctx)
+
+	// list resources
+	if err := cli.Client.List(ctx, resource, client.InNamespace(metav1.NamespaceAll)); err != nil {
+		return err
+	}
+
+	// marshal to yaml
+	o, err := yaml.Marshal(resource)
+	if err != nil {
+		return err
+	}
+
+	// print on stderr
+	if _, err := fmt.Fprintln(os.Stderr, string(o)); err != nil {
+		return err
+	}
+
+	return nil
+}
