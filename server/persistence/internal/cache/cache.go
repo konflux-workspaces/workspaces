3e8aa2ae822ad52fefae3b819f6faf7fbfb4cc79
Francesco Ilario <filario@redhat.com>
2024-05-15 08:48:09 +0200

    rest: add iwclient package (#127)
    * rest: add iwclient package
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * server: add internal package cache
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * op: update labels
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * reducing internal/cache complexity
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * add comments
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * cleanup tests
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ server/persistence/internal/cache/cache.go
@@ -0,0 +1,99 @@
+package cache
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+	workspacesv1alpha1 "github.com/konflux-workspaces/workspaces/operator/api/v1alpha1"
+)
+
+const (
+	LabelWorkspaceVisibility string = "workspaces.io/visibility"
+)
+
+// NewCache creates a controller-runtime cache.Cache instance configured to monitor
+// spacebindings.toolchain.dev.openshift.com and workspaces.workspaces.io.
+// IMPORTANT: returned cache needs to be started and initialized.
+func NewCache(ctx context.Context, cfg *rest.Config, workspacesNamespace, kubesawNamespace string) (cache.Cache, error) {
+	s, err := createScheme()
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := createMapper(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := newCache(cfg, s, m, workspacesNamespace, kubesawNamespace)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := c.GetInformer(ctx, &toolchainv1alpha1.SpaceBinding{}); err != nil {
+		return nil, err
+	}
+	if _, err := c.GetInformer(ctx, &workspacesv1alpha1.InternalWorkspace{}); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func newCache(cfg *rest.Config, scheme *runtime.Scheme, mapper meta.RESTMapper, workspacesNamespace, kubesawNamespace string) (cache.Cache, error) {
+	return cache.New(cfg, cache.Options{
+		Scheme:                      scheme,
+		Mapper:                      mapper,
+		ReaderFailOnMissingInformer: true,
+		ByObject: map[client.Object]cache.ByObject{
+			&toolchainv1alpha1.SpaceBinding{}:       {Namespaces: map[string]cache.Config{kubesawNamespace: {}}},
+			&workspacesv1alpha1.InternalWorkspace{}: {Namespaces: map[string]cache.Config{workspacesNamespace: {}}},
+		},
+		DefaultTransform: func(obj interface{}) (interface{}, error) {
+			if ws, ok := obj.(*workspacesv1alpha1.InternalWorkspace); ok {
+				if ws.Labels == nil {
+					ws.Labels = map[string]string{}
+				}
+				ws.Labels[LabelWorkspaceVisibility] = string(ws.Spec.Visibility)
+				return ws, nil
+			}
+
+			return obj, nil
+		},
+	})
+}
+
+func createMapper(cfg *rest.Config) (meta.RESTMapper, error) {
+	hc, err := rest.HTTPClientFor(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return apiutil.NewDynamicRESTMapper(cfg, hc)
+}
+
+func createScheme() (*runtime.Scheme, error) {
+	s := runtime.NewScheme()
+	if err := corev1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := metav1.AddMetaToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := workspacesv1alpha1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := toolchainv1alpha1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
---
+++ server/persistence/iwclient/iwclient.go
@@ -0,0 +1,49 @@
+package iwclient
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+)
+
+// SpaceKey comprises a Space name, with a mandatory owner.
+type SpaceKey struct {
+	Owner string
+	Name  string
+}
+
+type Client struct {
+	backend client.Reader
+
+	kubesawNamespace    string
+	workspacesNamespace string
+}
+
+// New creates a client that uses the provided backend as source
+func New(backend client.Reader, workspacesNamespace, kubesawNamespace string) *Client {
+	return &Client{
+		backend:             backend,
+		kubesawNamespace:    kubesawNamespace,
+		workspacesNamespace: workspacesNamespace,
+	}
+}
+
+func (c *Client) existsSpaceBindingForUserAndSpace(ctx context.Context, user, space string) (bool, error) {
+	ml := client.MatchingLabels{
+		toolchainv1alpha1.SpaceBindingMasterUserRecordLabelKey: user,
+		toolchainv1alpha1.SpaceBindingSpaceLabelKey:            space,
+	}
+	sbb := toolchainv1alpha1.SpaceBindingList{}
+	if err := c.backend.List(ctx, &sbb, ml); err != nil {
+		return false, err
+	}
+
+	return len(sbb.Items) > 0, nil
+}
+
+func (c *Client) workspaceNamespacedName(space string) client.ObjectKey {
+	return types.NamespacedName{Namespace: c.workspacesNamespace, Name: space}
+}
