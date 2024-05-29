6203636306e51a6375d78cda3754b1efe093dc3b
Francesco Ilario <filario@redhat.com>
2024-05-29 18:36:39 +0200

    remove toolchain_install.sh in favor of ci/toolchain_manager.sh
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/demo.sh
+++ hack/demo.sh
@@ -7,12 +7,14 @@ export QUAY_NAMESPACE=${QUAY_NAMESPACE:-workspaces}
 
 CURRENT_DIR="$(readlink -f "$0")"
 SCRIPT_DIR="$(dirname "${CURRENT_DIR}")"
+CI_DIR="$(realpath "$(dirname "${CURRENT_DIR}")/../ci")"
 
 SUFFIX="e2e$(date +'%d%H%M%S')"
 echo "using suffix: ${SUFFIX}"
 
 # build and install toolchain
-( "${SCRIPT_DIR}/toolchain_install.sh" "${SUFFIX}" )
+( "${CI_DIR}/toolchain_manager.sh" "publish" "${SUFFIX}" "-n" "${QUAY_NAMESPACE}" && \
+  "${CI_DIR}/toolchain_manager.sh" "deploy" "${SUFFIX}" "-n" "${QUAY_NAMESPACE}" )
 
 # build and install workspaces
 ( "${SCRIPT_DIR}/workspaces_install.sh" && make -C e2e test )
