19727b9df4b0dd7a62c3320ba9784e6f42c886a9
Francesco Ilario <filario@redhat.com>
2024-05-30 11:24:09 +0200

    update kyverno
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/install_kyverno.sh
+++ hack/install_kyverno.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-KYVERNO_VERSION=${KYVERNO_VERSION:-v1.10.0}
+KYVERNO_VERSION=${KYVERNO_VERSION:-v1.12.2}
 
 kubectl create -f "https://github.com/kyverno/kyverno/releases/download/$KYVERNO_VERSION/install.yaml" -n kyverno
