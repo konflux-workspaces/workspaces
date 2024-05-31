96ae8d497ed4e88cff77201c440b11e2e208c172
Francesco Ilario <filario@redhat.com>
2024-05-31 15:58:03 +0200

    remove install_kyverno.sh script
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/install_kyverno.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -o pipefail
-
-KYVERNO_VERSION="${KYVERNO_VERSION:-v1.12.2}"
-
-kubectl create \
-  -f "https://github.com/kyverno/kyverno/releases/download/${KYVERNO_VERSION}/install.yaml" \
-  -n kyverno
