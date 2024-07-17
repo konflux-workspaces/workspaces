0d4fe6282f1f3cf4b6fac0c6dc6a7b1680bc3dc0
Francesco Ilario <filario@redhat.com>
2024-07-17 21:23:37 +0200

    rest: add unit tests for read and list in rest pkg (#230)
    * rest: add unit tests for read and list in rest pkg
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Andy Sadler <ansadler@redhat.com>
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    Co-authored-by: Andy Sadler <ansadler@redhat.com>

---
+++ server/rest/workspace/list_test.go
@@ -0,0 +1,104 @@
+package workspace_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+
+	"github.com/konflux-workspaces/workspaces/server/rest/workspace/mocks"
+
+	coreworkspace "github.com/konflux-workspaces/workspaces/server/core/workspace"
+	"github.com/konflux-workspaces/workspaces/server/rest/marshal"
+	"github.com/konflux-workspaces/workspaces/server/rest/workspace"
+
+	restworkspacesv1alpha1 "github.com/konflux-workspaces/workspaces/server/api/v1alpha1"
+)
+
+var _ = Describe("List", func() {
+	var (
+		ctrl    *gomock.Controller
+		w       *restworkspacesv1alpha1.Workspace
+		request *http.Request
+		fake    *mocks.MockFakeResponseWriter
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		w = &restworkspacesv1alpha1.Workspace{}
+		w.Name = "foo"
+		w.Namespace = "bar"
+
+		request = buildListRequest(w)
+		fake = mocks.NewMockFakeResponseWriter(ctrl)
+	})
+
+	AfterEach(func() { ctrl.Finish() })
+
+	DescribeTable("workspace GET handler: read",
+		func(
+			mapperFunc workspace.ListWorkspaceMapperFunc,
+			readHandler workspace.ListWorkspaceQueryHandlerFunc,
+			marshaler marshal.MarshalerProvider,
+			responseFunc func() http.ResponseWriter,
+		) {
+			response := responseFunc()
+			handler := workspace.NewListWorkspaceHandler(mapperFunc, readHandler, marshaler)
+			handler.ServeHTTP(response, request)
+		},
+		Entry("failure in marshal provider", workspace.MapListWorkspaceHttp, nopListHandler, errorMarshalProvider, func() http.ResponseWriter {
+			fake.EXPECT().WriteHeader(http.StatusBadRequest)
+			return fake
+		}),
+		Entry("failure in read handler", workspace.MapListWorkspaceHttp, badListHandler, marshal.DefaultMarshalerProvider, func() http.ResponseWriter {
+			fake.EXPECT().WriteHeader(http.StatusInternalServerError)
+			return fake
+		}),
+		Entry("failure marshaling response", workspace.MapListWorkspaceHttp, nopListHandler, badMarshalProvider, func() http.ResponseWriter {
+			fake.EXPECT().WriteHeader(http.StatusInternalServerError)
+			return fake
+		}),
+		Entry("failure to write response", workspace.MapListWorkspaceHttp, nopListHandler, marshal.DefaultMarshalerProvider, func() http.ResponseWriter {
+			fake.EXPECT().Header().Return(http.Header{})
+			fake.EXPECT().Write(gomock.Any()).Return(0, fmt.Errorf("failed to write response body"))
+			fake.EXPECT().WriteHeader(http.StatusInternalServerError)
+			return fake
+		}),
+		Entry("failure to write response", workspace.MapListWorkspaceHttp, nopListHandler, marshal.DefaultMarshalerProvider, func() http.ResponseWriter {
+			fake.EXPECT().Header().Return(http.Header{})
+			fake.EXPECT().Write(gomock.Any()).DoAndReturn(func(a any) (int, error) {
+				slice, ok := a.([]byte)
+				Expect(ok).To(BeTrue())
+				return len(slice), nil
+			})
+			return fake
+		}),
+	)
+})
+
+func badListHandler(ctx context.Context, cmd coreworkspace.ListWorkspaceQuery) (*coreworkspace.ListWorkspaceResponse, error) {
+	return nil, fmt.Errorf("bad create handler")
+}
+
+func nopListHandler(_ctx context.Context, cmd coreworkspace.ListWorkspaceQuery) (*coreworkspace.ListWorkspaceResponse, error) {
+	return &coreworkspace.ListWorkspaceResponse{
+		Workspaces: restworkspacesv1alpha1.WorkspaceList{},
+	}, nil
+}
+
+func buildListRequest(workspace *restworkspacesv1alpha1.Workspace) *http.Request {
+	byteSlice, err := marshal.DefaultMarshal.Marshal(workspace)
+	Expect(err).NotTo(HaveOccurred())
+
+	url := fmt.Sprintf("/apis/workspaces.io/v1alpha1/namespaces/%s/workspaces", workspace.GetNamespace())
+
+	request, err := http.NewRequest(http.MethodGet, url, bytes.NewReader(byteSlice))
+	Expect(err).NotTo(HaveOccurred())
+	request.Header.Add("Content-Type", marshal.DefaultUnmarshal.ContentType())
+	request.Header.Add("Accept", marshal.DefaultMarshal.ContentType())
+	return request
+}
