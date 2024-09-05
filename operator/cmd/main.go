178af81691dcc30760b3bde762cae18eda5f4b41
Andy Sadler <ansadler@redhat.com>
2024-09-05 14:04:38 -0500

    remove kube-rbac-proxy as a dependency
    We need to set up a ServiceMonitor for our service.  We could use
    kube-rbac-proxy, but that has apparently been deprecated by kubebuilder,
    and they recommend moving away from it.  Instead, we can do the
    authentication and authorization checks inside our metrics server.
    
    To do this, we need to set up the ServiceMonitor with a ServiceAccount
    with permissions to talk to `/metrics` from our metrics service.
    
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -31,6 +31,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -86,7 +87,8 @@ func main() {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,
 		Metrics: metricsserver.Options{
-			BindAddress: metricsAddr,
+			BindAddress:    metricsAddr,
+			FilterProvider: filters.WithAuthenticationAndAuthorization,
 		},
 		WebhookServer: webhook.NewServer(webhook.Options{
 			Port: 944,
