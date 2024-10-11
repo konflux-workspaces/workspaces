6c0ed76a589f906ef0c7f64031b67337ed7ca2ac
Francesco Ilario <filario@redhat.com>
2024-10-11 17:00:39 +0200

    rest: add initial support for PATCH
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ server/core/workspace/workspace_patch.go
@@ -0,0 +1,106 @@
+package workspace
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	jsonpatch "github.com/evanphx/json-patch/v5"
+	ccontext "github.com/konflux-workspaces/workspaces/server/core/context"
+	"github.com/konflux-workspaces/workspaces/server/log"
+
+	restworkspacesv1alpha1 "github.com/konflux-workspaces/workspaces/server/api/v1alpha1"
+	workspacesv1alpha1 "github.com/konflux-workspaces/workspaces/server/api/v1alpha1"
+)
+
+// PatchWorkspaceCommand contains the information needed to retrieve a Workspace the user has access to from the data source
+type PatchWorkspaceCommand struct {
+	Owner     string
+	Workspace string
+	Patch     []byte
+	PatchType types.PatchType
+}
+
+// PatchWorkspaceResponse contains the workspace the user requested
+type PatchWorkspaceResponse struct {
+	Workspace *restworkspacesv1alpha1.Workspace
+}
+
+// PatchWorkspaceHandler processes PatchWorkspaceCommand and returns PatchWorkspaceResponse fetching data from a WorkspacePatcher
+type PatchWorkspaceHandler struct {
+	reader  WorkspaceReader
+	updater WorkspaceUpdater
+}
+
+// NewPatchWorkspaceHandler creates a new PatchWorkspaceHandler that uses a specified WorkspacePatcher
+func NewPatchWorkspaceHandler(reader WorkspaceReader, updater WorkspaceUpdater) *PatchWorkspaceHandler {
+	return &PatchWorkspaceHandler{
+		reader:  reader,
+		updater: updater,
+	}
+}
+
+// Handle handles a PatchWorkspaceCommand and returns a PatchWorkspaceResponse or an error
+func (h *PatchWorkspaceHandler) Handle(ctx context.Context, command PatchWorkspaceCommand) (*PatchWorkspaceResponse, error) {
+	// authorization
+	// If required, implement here complex logic like multiple-domains filtering, etc
+	u, ok := ctx.Value(ccontext.UserSignupComplaintNameKey).(string)
+	if !ok {
+		return nil, fmt.Errorf("unauthenticated request")
+	}
+
+	// validate query
+	// TODO: sanitize input, block reserved labels, etc
+
+	// retrieve workspace
+	w := workspacesv1alpha1.Workspace{}
+	if err := h.reader.ReadUserWorkspace(ctx, u, command.Owner, command.Workspace, &w); err != nil {
+		return nil, err
+	}
+
+	// apply patch
+	pw, err := h.applyPatch(&w, command)
+	if err != nil {
+		return nil, fmt.Errorf("error patching Workspace %s/%s: %w", command.Owner, command.Workspace, err)
+	}
+
+	log.FromContext(ctx).Debug("updating workspace", "workspace", pw)
+	opts := &client.UpdateOptions{}
+	if err := h.updater.UpdateUserWorkspace(ctx, u, pw, opts); err != nil {
+		return nil, err
+	}
+
+	// reply
+	return &PatchWorkspaceResponse{
+		Workspace: pw,
+	}, nil
+}
+
+func (h *PatchWorkspaceHandler) applyPatch(w *workspacesv1alpha1.Workspace, command PatchWorkspaceCommand) (*workspacesv1alpha1.Workspace, error) {
+	if command.PatchType != types.MergePatchType {
+		return nil, fmt.Errorf("unsupported patch type: %s", command.PatchType)
+	}
+
+	// marshal workspace as json
+	wj, err := json.Marshal(w)
+	if err != nil {
+		return nil, err
+	}
+
+	// apply jsonpatch
+	pwj, err := jsonpatch.MergePatch(wj, []byte(command.Patch))
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarshal json to struct
+	pw := workspacesv1alpha1.Workspace{}
+	if err := json.Unmarshal(pwj, &pw); err != nil {
+		return nil, err
+	}
+
+	return &pw, nil
+}
---
+++ server/rest/workspace/patch.go
@@ -0,0 +1,140 @@
+package workspace
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/konflux-workspaces/workspaces/server/core"
+	"github.com/konflux-workspaces/workspaces/server/core/workspace"
+	"github.com/konflux-workspaces/workspaces/server/log"
+	"github.com/konflux-workspaces/workspaces/server/rest/header"
+	"github.com/konflux-workspaces/workspaces/server/rest/marshal"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var (
+	_ http.Handler = &PatchWorkspaceHandler{}
+
+	_ PatchWorkspaceMapperFunc = MapPatchWorkspaceHttp
+)
+
+// handler dependencies
+type PatchWorkspaceMapperFunc func(*http.Request) (*workspace.PatchWorkspaceCommand, error)
+type PatchWorkspaceCommandHandlerFunc func(context.Context, workspace.PatchWorkspaceCommand) (*workspace.PatchWorkspaceResponse, error)
+
+// PatchWorkspaceHandler the http.Request handler for Patch Workspaces endpoint
+type PatchWorkspaceHandler struct {
+	MapperFunc     PatchWorkspaceMapperFunc
+	CommandHandler PatchWorkspaceCommandHandlerFunc
+
+	MarshalerProvider marshal.MarshalerProvider
+}
+
+// NewPatchWorkspaceHandler creates a PatchWorkspaceHandler
+func NewDefaultPatchWorkspaceHandler(
+	handler PatchWorkspaceCommandHandlerFunc,
+) *PatchWorkspaceHandler {
+	return NewPatchWorkspaceHandler(
+		MapPatchWorkspaceHttp,
+		handler,
+		marshal.DefaultMarshalerProvider,
+	)
+}
+
+// NewPatchWorkspaceHandler creates a PatchWorkspaceHandler
+func NewPatchWorkspaceHandler(
+	mapperFunc PatchWorkspaceMapperFunc,
+	queryHandler PatchWorkspaceCommandHandlerFunc,
+	marshalerProvider marshal.MarshalerProvider,
+) *PatchWorkspaceHandler {
+	return &PatchWorkspaceHandler{
+		MapperFunc:        mapperFunc,
+		CommandHandler:    queryHandler,
+		MarshalerProvider: marshalerProvider,
+	}
+}
+
+func (h *PatchWorkspaceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	l := log.FromContext(r.Context())
+	l.Debug("executing patch")
+
+	// build marshaler for the given request
+	l.Debug("building marshaler for request")
+	m, err := h.MarshalerProvider(r)
+	if err != nil {
+		l.Debug("error building marshaler for request", "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// map
+	l.Debug("mapping request to patch command")
+	c, err := h.MapperFunc(r)
+	if err != nil {
+		l.Debug("error mapping request to command", "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// execute
+	l.Debug("executing patch command", "command", c)
+	cr, err := h.CommandHandler(r.Context(), *c)
+	if err != nil {
+		l = l.With("error", err)
+		switch {
+		case errors.Is(err, core.ErrNotFound):
+			l.Debug("error executing patch command: resource not found")
+			w.WriteHeader(http.StatusNotFound)
+		default:
+			l.Error("error executing patch command")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// marshal response
+	l.Debug("marshaling response", "response", &cr)
+	d, err := m.Marshal(cr.Workspace)
+	if err != nil {
+		l.Error("unexpected error marshaling response", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// reply
+	l.Debug("writing response", "response", d)
+	w.Header().Add(header.ContentType, m.ContentType())
+	if _, err := w.Write(d); err != nil {
+		l.Error("unexpected error writing response", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func MapPatchWorkspaceHttp(r *http.Request) (*workspace.PatchWorkspaceCommand, error) {
+	// parse request body
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading request body: %w", err)
+	}
+
+	ct, ok := r.Header["Content-Type"]
+	if !ok || len(ct) != 1 {
+		return nil, fmt.Errorf("Content-Type header is required")
+	}
+
+	// retrieve namespace from path
+	n := r.PathValue("name")
+	ns := r.PathValue("namespace")
+
+	// build command
+	return &workspace.PatchWorkspaceCommand{
+		Workspace: n,
+		Owner:     ns,
+		PatchType: types.PatchType(ct[0]),
+		Patch:     d,
+	}, nil
+}
